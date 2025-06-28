d19e0f40b0f298a6fbac6d16d4baa2aaaa9ed833
Michael Wang <a0416517630@icloud.com>
2025-06-28 21:28:49 +1000

    changed name

---
+++ app.py
@@ -0,0 +1,142 @@
+import os
+import yaml 
+import requests
+import webbrowser
+import pandas as pd 
+import streamlit as st
+from io import BytesIO 
+from dotenv import load_dotenv
+import matplotlib.pyplot as plt 
+import matplotlib.ticker as ticker
+from rmb_converter import to_rmb_upper
+import streamlit_authenticator as stauth
+
+load_dotenv()
+receipt_url = os.getenv("receipt_url")
+contract_url = os.getenv("contract_url")
+data_url = os.getenv("data_url")
+receipt_download = os.getenv("receipt_download")
+
+# load config
+with open('config.yaml') as file:
+    config = yaml.safe_load(file)
+
+# @st.cache_data
+def load_excel(url):
+    response = requests.get(url)
+    return pd.read_excel(BytesIO(response.content), sheet_name="Analytics")
+
+def millions(x, pos):
+    return f'{x*1e-6:.1f}M'  # converts 10000000 to 10.0M
+
+authenticator = stauth.Authenticate(
+    credentials=config['credentials'],
+    cookie_name=config['cookie']['name'],
+    key=config['cookie']['key'],
+    cookie_expiry_days=config['cookie']['expiry_days']
+)
+
+def open_doc(link):
+    webbrowser.open(link)
+
+# Render login widget
+authenticator.login()
+
+# Main control flow
+auth_status = st.session_state.get("authentication_status")
+
+if "page" not in st.session_state:
+    st.session_state.page = "main"
+
+if auth_status:
+    st.title(f"{st.session_state['name']}'s Work Space")
+
+    # SIDE BAR
+    with st.sidebar:
+        st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
+        st.sidebar.title("Options")
+
+        # sec 1
+        if st.sidebar.button("Main"):
+            st.session_state.page = "main"
+            st.rerun()
+        elif st.sidebar.button("Analytics"):
+            st.session_state.page = "analytics"
+            st.rerun()
+    
+    authenticator.logout("Logout", "sidebar")
+
+    if st.session_state.page == "main":
+        # document files
+        st.subheader("Documents")
+        if st.button("Receipt"):
+            open_doc(receipt_url)
+
+        if st.button("Contract"):
+            open_doc(contract_url)
+
+        st.subheader("Rules")
+        st.markdown(
+            f"""
+            <style>
+            iframe {{
+                overflow: hidden !important;
+                scrollbar-width: none !important;  /* Firefox */
+            }}
+            iframe::-webkit-scrollbar {{
+                display: none !important;  /* Chrome, Safari */
+            }}
+            </style>
+
+            <iframe src="{data_url}" width="100%" height="800px" style="max-width: 900px; border: none; overflow: hidden;"></iframe>
+            """,
+            unsafe_allow_html=True
+        )
+
+        # rmb converter
+        st.subheader("RMB Converter")
+        number = st.text_input("Enter a number", "")
+        # Convert button
+        if st.button("Convert"):
+            if number.isdigit():
+                chinese_result = to_rmb_upper(int(number))
+                st.success(f"Chinese Numerals: {chinese_result}")
+            else:
+                st.error("Please enter a valid positive integer.")
+
+    elif st.session_state.page == "analytics":
+
+        df = load_excel(receipt_download)
+
+        # 1. Show raw data preview
+        st.write("## Raw Data Preview")
+        st.dataframe(df)
+
+        st.write('## Company by Revenue')
+        df_sorted = df.sort_values(by='收入', ascending=True)
+        df_sorted['公司'] = df_sorted['公司'].apply(lambda x: x[:-4])
+
+        # Set Chinese font before plotting
+        plt.rcParams['font.sans-serif'] = ['SimHei']  
+        plt.rcParams['axes.unicode_minus'] = False  # fix minus sign issue
+
+        # Create the plot
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.yaxis.set_major_formatter(ticker.FuncFormatter(millions))
+        df_sorted.plot(x="公司", y="收入", kind="bar", ax=ax)
+
+        # Rotate x labels for better readability
+        plt.xticks(rotation=45)
+
+        # Label axes
+        plt.xlabel("公司")
+        plt.ylabel("收入")
+
+        # Display the plot in Streamlit
+        st.pyplot(fig)
+
+elif auth_status is False:
+    st.error("Username or password is incorrect.")
+
+else:
+    st.warning("Please log in to continue.")
